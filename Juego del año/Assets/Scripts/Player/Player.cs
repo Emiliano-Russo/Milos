84c2ce62457c59c94897a48997c4e79d1e7387d2
Emiliano <russo.emiliano97@gmail.com>
2020-07-21 18:20:38 -0300

    Variado (leer decrp)
    Se agrego un barra de gayStatus bien hecha, controlada por la clase player
    Se quito la clase UiSpawner y se puso todo en PlayerSpawn
    Se añadieron sonidos de la noche

--- Juego del año/Assets/Scripts/Player/Player.cs
+++ Juego del año/Assets/Scripts/Player/Player.cs
@@ -3,36 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Hashtable = ExitGames.Client.Photon.Hashtable;
+//using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class Player : MonoBehaviour
 {
 
     public Behaviour[] DisableThings;
-    public PhotonView pv;
-    public bool EnEntornoDePrueba = false;
 
 
-
-    private float life = 100;
-    private Text lifeStats;
-    private Text ping;
-    private Hashtable personalHash = new Hashtable();
+    private PhotonView myPhotonView;
+    private float gayStatusValue;
+    private Slider gayStatusSlider;
+    private Text pingTxt;
 
 
     private void Start()
     {
-        if (pv.IsMine || EnEntornoDePrueba)
-        {
-            lifeStats = GameObject.FindGameObjectWithTag("lifeStats").GetComponent<Text>();
-            ping = GameObject.FindGameObjectWithTag("ping").GetComponent<Text>();
-            personalHash[PhotonNetwork.NickName] = this.life;
-            PhotonNetwork.SetPlayerCustomProperties(personalHash);
-            return;
-        }
-        else
-        {
-            //desactivo mis cosas (sino soy local)
+        Cursor.lockState = CursorLockMode.Locked;
+        gayStatusSlider = GameObject.FindGameObjectWithTag("gayStatus").GetComponent<Slider>();
+        pingTxt = GameObject.FindGameObjectWithTag("ping").GetComponent<Text>();
+        gayStatusValue = 10;
+        gayStatusSlider.value = gayStatusValue;
+        myPhotonView = this.GetComponent<PhotonView>();
+        if (!myPhotonView.IsMine)
+        { 
+            //disable my things (if im not localPlayer)
             foreach (var item in DisableThings)
                 item.gameObject.SetActive(false);
         }
@@ -40,28 +35,10 @@ public class Player : MonoBehaviour
 
     public void Update()
     {
-        if (!pv.IsMine && !EnEntornoDePrueba)
+        if (!myPhotonView.IsMine)
             return;
-        ping.text = NetworkManager.instance.GetPing().ToString() + " ms";
-        GetAllPlayersHeal();
-
-        if (Input.GetKeyDown(KeyCode.O))
-            TakeDamage(20);
+        pingTxt.text = NetworkManager.instance.GetPing().ToString() + " ms";
     }
 
-    public void TakeDamage(float amount)
-    {
-        personalHash[PhotonNetwork.NickName] = (float)personalHash[PhotonNetwork.NickName] - amount;
-        PhotonNetwork.SetPlayerCustomProperties(personalHash);
-    }
-
-    public void GetAllPlayersHeal()
-    {
-        var players = NetworkManager.instance.GetPlayerList();
-        lifeStats.text = NetworkManager.instance.GetNickName() + ": " + personalHash[PhotonNetwork.NickName];
-
-        foreach (var player in players)
-            lifeStats.text += "-" + player.NickName + player.CustomProperties[player.NickName];        
-    }
 
 }
--- Juego del año/Assets/Scripts/Player/PlayerSpawn.cs
+++ Juego del año/Assets/Scripts/Player/PlayerSpawn.cs
@@ -11,11 +11,15 @@ public class PlayerSpawn : MonoBehaviour
     [SerializeField]
     private GameObject spawnLocation;
 
+    [SerializeField]
+    private Canvas canvas;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.Instantiate(player.name, spawnLocation.transform.position, Quaternion.identity);
+        Instantiate(canvas);
     }
 }
